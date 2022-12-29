59ad6735130b1b7d31c3be28230bb30c6c3c27f3
larg10 <a20310018@ceti.mx>
2022-12-29 17:53:25 -0600

    Login diseño y bttn "¿no tienes cuenta?"

--- app/src/main/java/com/example/apprappi/Login.kt
+++ app/src/main/java/com/example/apprappi/Login.kt
@@ -1,11 +1,19 @@
 package com.example.apprappi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun bttnRegister(view: View){
+        val intent = Intent(this@Login, Register::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/apprappi/MainActivity.kt
+++ app/src/main/java/com/example/apprappi/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.apprappi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                val intent = Intent(this@MainActivity, Login::class.java)
+                startActivity(intent)
+            }
+        }, 1500)
     }
 }
